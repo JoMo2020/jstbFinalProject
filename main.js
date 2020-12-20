076bc1952df22b17a77f7f53f6b74effd0e88eb3
JoMo2020 <jastbernard@gmail.com>
2020-12-20 19:25:23 -0400

    add var myList and var unique to main.js

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-var moment = require('moment'); // require 
-var myDate = new Date();
+var moment = require('moment')
+var unique = require('uniq');
 
-console.log(myDate);
+var myDate = new Date();
+var myCoolDate = moment(myDate).format('LL');
+
+var myList = [1, 2, 1, 1, 1, 3, 3, 4, 5, 6, 4, 4, 7];
+
+var myUniqueList = unique(myList);
+
+console.log(myUniqueList);
